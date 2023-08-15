6ed4d699f0d6cba502221a943649b9fbd5c0ed97
Samuel Soares Guimarães <34168199+samuelsoares27@users.noreply.github.com>
2023-08-15 20:18:44 -0300

    Add project files.

---
+++ Implicit&Explict_c#/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+int valueInteger = 10;
+//This explict casting is something temporary
+double valueDouble = (double)valueInteger;
+Console.WriteLine(valueDouble);
+
+// This is implicit casting because the compiler know the double
+// have more space to receive the integer, it's automatic
+double valueDoubleImplicitCompiler = valueInteger;
+Console.WriteLine(valueDoubleImplicitCompiler);
+
+double valueDoubleimplicitFunction = Convert.ToDouble(valueInteger);
+Console.WriteLine(valueDoubleimplicitFunction);
+
